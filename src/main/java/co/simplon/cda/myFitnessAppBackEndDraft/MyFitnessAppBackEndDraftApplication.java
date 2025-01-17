57740f1d4708e1ee8061524fae7c61000d0dd019
Thomas <thomas.heroguelle@renault.com>
2025-01-17 15:49:12 +0100

    Init project

---
+++ src/main/java/co/simplon/cda/myFitnessAppBackEndDraft/MyFitnessAppBackEndDraftApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.cda.myFitnessAppBackEndDraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessAppBackEndDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessAppBackEndDraftApplication.class, args);
+	}
+
+}
